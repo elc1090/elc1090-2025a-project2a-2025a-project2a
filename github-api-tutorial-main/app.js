a66caf37080f5e9bb910bd6de479624578560545
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2025-04-10 14:26:44 -0300

    💡 refactor: translate comments

--- github-api-tutorial-main/app.js
+++ github-api-tutorial-main/app.js
@@ -1,64 +1,64 @@
-// Get the GitHub username input form
-const gitHubForm = document.getElementById('gitHubForm');
+// Obtém o formulário de entrada do nome de usuário do GitHub
+const gitHubForm = document.getElementById("gitHubForm");
 
-// Listen for submissions on GitHub username input form
-gitHubForm.addEventListener('submit', (e) => {
+// Escuta por envios no formulário de nome de usuário do GitHub
+gitHubForm.addEventListener("submit", (e) => {
+  // Previne a ação padrão de envio do formulário
+  e.preventDefault();
 
-    // Prevent default form submission action
-    e.preventDefault();
+  // Obtém o campo de entrada do nome de usuário do GitHub no DOM
+  let usernameInput = document.getElementById("usernameInput");
 
-    // Get the GitHub username input field on the DOM
-    let usernameInput = document.getElementById('usernameInput');
+  // Obtém o valor do campo de entrada do nome de usuário do GitHub
+  let gitHubUsername = usernameInput.value;
 
-    // Get the value of the GitHub username input field
-    let gitHubUsername = usernameInput.value;
+  // Executa a função da API do GitHub, passando o nome de usuário do GitHub
+  requestUserRepos(gitHubUsername)
+    .then((response) => response.json()) // converte a resposta em json
+    .then((data) => {
+      // atualiza o html com os dados do github
+      for (let i in data) {
+        // Obtém a ul com o id userRepos
 
-    // Run GitHub API function, passing in the GitHub username
-    requestUserRepos(gitHubUsername)
-        .then(response => response.json()) // parse response into json
-        .then(data => {
-            // update html with data from github
-            for (let i in data) {
-                // Get the ul with id of userRepos
+        if (data.message === "Not Found") {
+          let ul = document.getElementById("userRepos");
 
-                if (data.message === "Not Found") {
-                    let ul = document.getElementById('userRepos');
+          // Cria variável que criará li's para serem adicionadas à ul
+          let li = document.createElement("li");
 
-                    // Create variable that will create li's to be added to ul
-                    let li = document.createElement('li');
+          // Adiciona a classe de item de lista do Bootstrap a cada li
+          li.classList.add("list-group-item");
+          // Cria a marcação html para cada li
+          li.innerHTML = `
+                <p><strong>Não existe conta com o usuário:</strong> ${gitHubUsername}</p>`;
+          // Adiciona cada li à ul
+          ul.appendChild(li);
+        } else {
+          let ul = document.getElementById("userRepos");
 
-                    // Add Bootstrap list item class to each li
-                    li.classList.add('list-group-item')
-                    // Create the html markup for each li
-                    li.innerHTML = (`
-                <p><strong>No account exists with username:</strong> ${gitHubUsername}</p>`);
-                    // Append each li to the ul
-                    ul.appendChild(li);
-                } else {
+          // Cria variável que criará li's para serem adicionadas à ul
+          let li = document.createElement("li");
 
-                    let ul = document.getElementById('userRepos');
+          // Adiciona a classe de item de lista do Bootstrap a cada li
+          li.classList.add("list-group-item");
 
-                    // Create variable that will create li's to be added to ul
-                    let li = document.createElement('li');
-
-                    // Add Bootstrap list item class to each li
-                    li.classList.add('list-group-item')
-
-                    // Create the html markup for each li
-                    li.innerHTML = (`
-                <p><strong>Repo:</strong> ${data[i].name}</p>
-                <p><strong>Description:</strong> ${data[i].description}</p>
+          // Cria a marcação html para cada li
+          li.innerHTML = `
+                <p><strong>Repositório:</strong> ${data[i].name}</p>
+                <p><strong>Descrição:</strong> ${data[i].description}</p>
                 <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
-            `);
+            `;
 
-                    // Append each li to the ul
-                    ul.appendChild(li);
-                }
-            }
-        })
-})
+          // Adiciona cada li à ul
+          ul.appendChild(li);
+        }
+      }
+    });
+});
 
 function requestUserRepos(username) {
-    // create a variable to hold the `Promise` returned from `fetch`
-    return Promise.resolve(fetch(`https://api.github.com/users/${username}/repos`));
+  // cria uma variável para conter a `Promise` retornada de `fetch`
+  return Promise.resolve(
+    fetch(`https://api.github.com/users/${username}/repos`)
+  );
 }
